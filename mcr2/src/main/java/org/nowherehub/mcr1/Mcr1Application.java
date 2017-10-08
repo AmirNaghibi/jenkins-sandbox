93a65cfea99af437f5ad78fe2bd3f8a93dbc30a8
Sylvere Richard <sylvere.richard@gmail.com>
2017-10-08 21:01:46 +0200

    add 3 projects

---
+++ mcr2/src/main/java/org/nowherehub/mcr1/Mcr1Application.java
@@ -0,0 +1,12 @@
+package org.nowherehub.mcr3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mcr1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mcr1Application.class, args);
+	}
+}
