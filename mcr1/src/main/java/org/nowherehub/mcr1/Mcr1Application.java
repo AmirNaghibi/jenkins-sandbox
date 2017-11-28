7071423cbc5fe6abf7fe05b95da6d30fa79d1502
Sylvere Richard <sylvere.richard@gmail.com>
2017-11-28 18:41:28 +0100

    upgrade to spring-boot 1.5.9

--- mcr1/src/main/java/org/nowherehub/mcr1/Mcr1Application.java
+++ mcr1/src/main/java/org/nowherehub/mcr1/Mcr1Application.java
@@ -1,4 +1,4 @@
-package org.nowherehub.mcr3;
+package org.nowherehub.mcr1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
