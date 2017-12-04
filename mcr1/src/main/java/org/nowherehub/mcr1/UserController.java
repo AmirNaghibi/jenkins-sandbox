27d62f31671bf3cef522f5d70303d9b93b80862b
Sylvere Richard <sylvere.richard@gmail.com>
2017-12-04 23:07:29 +0100

    add querydsl

--- mcr1/src/main/java/org/nowherehub/mcr1/UserController.java
+++ mcr1/src/main/java/org/nowherehub/mcr1/UserController.java
@@ -1,9 +1,11 @@
 package org.nowherehub.mcr1;
 
 import com.google.common.collect.Lists;
+import com.querydsl.core.types.Predicate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.querydsl.binding.QuerydslPredicate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,8 +26,8 @@ public class UserController {
 
     @GetMapping
     @RequestMapping("/search")
-    public Page<User> getPageUser(Pageable page) {
-        return userRepository.findAll(page);
+    public Page<User> getPageUser(@QuerydslPredicate(root = User.class) Predicate predicate, Pageable page) {
+        return userRepository.findAll(predicate, page);
     }
 
 
--- mcr1/src/main/java/org/nowherehub/mcr1/UserRepository.java
+++ mcr1/src/main/java/org/nowherehub/mcr1/UserRepository.java
@@ -1,8 +1,9 @@
 package org.nowherehub.mcr1;
 
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+public interface UserRepository extends PagingAndSortingRepository<User, Long>, QuerydslPredicateExecutor<User> {
 }
