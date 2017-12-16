9221e95c5bca6fc6d0bba46e401f15d2f4787d05
Sylvere Richard <sylvere.richard@gmail.com>
2017-12-16 23:42:10 +0100

    add OAuth2 login

--- mcr1/src/main/java/org/nowherehub/mcr1/Mcr1Application.java
+++ mcr1/src/main/java/org/nowherehub/mcr1/Mcr1Application.java
@@ -2,11 +2,15 @@ package org.nowherehub.mcr1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
+@Import(Mcr1SecurityConfig.class)
 public class Mcr1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Mcr1Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Mcr1Application.class, args);
+    }
 }
